2842623be51b11995873005619b1fc05239544b0
Scribhneoir <scribhneoir21@gmail.com>
2021-04-27 16:35:10 -0700

    Updates

---
+++ static/js/app.751e3f7e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonp=this.webpackJsonp||[]).push([[0],{28:function(t,e,n){"use strict";n.d(e,"a",(function(){return i}));var a=n(52),r=n(0),o=n.n(r),c=n(3),s=n(45),u=n(11);function i(){return o.a.createElement(u.a,{style:p.container},o.a.createElement(s.a,null,"Open up App.tsx to start working on your app"),o.a.createElement(a.a,{style:"auto"}))}var p=c.a.create({container:{flex:1,backgroundColor:"#fff",alignItems:"center",justifyContent:"center"}})},53:function(t,e,n){t.exports=n(66)}},[[53,1,2]]]);
+//# sourceMappingURL=app.751e3f7e.chunk.js.map
