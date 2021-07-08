d2aff76c994f4275035882350767dfea102345f0
scribhneoir <scribhneoir21@gmail.com>
2021-07-08 01:36:32 +0000

    Updates

--- static/js/app.74b2c652.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[0],{28:function(e,t,n){"use strict";n.d(t,"a",(function(){return s}));var a=n(52),r=n(0),c=n.n(r),i=n(3),o=n(45),l=n(11);function s(){return c.a.createElement(l.a,{style:u.container},c.a.createElement(o.a,null,"Emotive will be available within the next year..."),c.a.createElement(a.a,{style:"auto"}))}var u=i.a.create({container:{flex:1,backgroundColor:"#fff",alignItems:"center",justifyContent:"center"}})},53:function(e,t,n){e.exports=n(66)}},[[53,1,2]]]);
-//# sourceMappingURL=app.74b2c652.chunk.js.map
