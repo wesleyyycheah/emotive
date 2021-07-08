7dec9d41ab4dcb537b5fda0a75013790ff3ac1c5
Josiah McCracken <josiah.m@boostpoint.com>
2021-07-07 21:35:30 -0400

    Test CNAME fix

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Emotive will be available within the next year...</Text>
+      <Text>Emotive will be available within the next year!</Text>
       <StatusBar style="auto" />
     </View>
   );
