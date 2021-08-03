6fed2436ac33252c190f8be9cd1ab228111ec124
scribhneoir <scribhneoir21@gmail.com>
2021-08-02 22:05:08 -0400

    Added all face components for v1

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -14,58 +14,217 @@ interface StyledProps {
   size: number;
   left: number;
   top: number;
+  type: number;
 }
+const partsModifier = {
+  hair: [
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+  ],
+  head: [
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+  ],
+  ear: [
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+  ],
+  eye: [
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+  ],
+  nose: [
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+  ],
+  mouth: [
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+    { height: 0, width: 0, top: 0, left: 0 },
+  ],
+};
 
 let sizeM = 3;
 
 let NoseC = styled.View<StyledProps>`
   position: absolute;
-  height: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  width: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  left: ${(props) => PixelRatio.roundToNearestPixel(props.left * sizeM)}px;
-  top: ${(props) => PixelRatio.roundToNearestPixel(props.top * sizeM)}px;
+  height: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.nose[props.type].height) * sizeM,
+    )}px;
+  width: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.nose[props.type].width) * sizeM,
+    )}px;
+  left: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.left + partsModifier.nose[props.type].left) * sizeM,
+    )}px;
+  top: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.top + partsModifier.nose[props.type].top) * sizeM,
+    )}px;
 `;
 let EyeL = styled.View<StyledProps>`
   position: absolute;
-  height: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  width: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  left: ${(props) => PixelRatio.roundToNearestPixel(props.left * sizeM)}px;
-  top: ${(props) => PixelRatio.roundToNearestPixel(props.top * sizeM)}px;
+  height: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.eye[props.type].height) * sizeM,
+    )}px;
+  width: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.eye[props.type].width) * sizeM,
+    )}px;
+  left: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.left + partsModifier.eye[props.type].left) * sizeM,
+    )}px;
+  top: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.top + partsModifier.eye[props.type].top) * sizeM,
+    )}px;
 `;
 let EyeR = styled.View<StyledProps>`
   position: absolute;
-  height: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  width: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  left: ${(props) => PixelRatio.roundToNearestPixel(props.left * sizeM)}px;
-  top: ${(props) => PixelRatio.roundToNearestPixel(props.top * sizeM)}px;
+  height: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.eye[props.type].height) * sizeM,
+    )}px;
+  width: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.eye[props.type].width) * sizeM,
+    )}px;
+  left: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.left + partsModifier.eye[props.type].left) * sizeM,
+    )}px;
+  top: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.top + partsModifier.eye[props.type].top) * sizeM,
+    )}px;
 `;
 let HeadC = styled.View<StyledProps>`
   position: absolute;
-  height: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  width: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  left: ${(props) => PixelRatio.roundToNearestPixel(props.left * sizeM)}px;
-  top: ${(props) => PixelRatio.roundToNearestPixel(props.top * sizeM)}px;
+  height: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.head[props.type].height) * sizeM,
+    )}px;
+  width: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.head[props.type].width) * sizeM,
+    )}px;
+  left: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.left + partsModifier.head[props.type].left) * sizeM,
+    )}px;
+  top: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.top + partsModifier.head[props.type].top) * sizeM,
+    )}px;
 `;
 let MouthC = styled.View<StyledProps>`
   position: absolute;
-  height: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  width: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  left: ${(props) => PixelRatio.roundToNearestPixel(props.left * sizeM)}px;
-  top: ${(props) => PixelRatio.roundToNearestPixel(props.top * sizeM)}px;
+  height: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.mouth[props.type].height) * sizeM,
+    )}px;
+  width: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.mouth[props.type].width) * sizeM,
+    )}px;
+  left: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.left + partsModifier.mouth[props.type].left) * sizeM,
+    )}px;
+  top: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.top + partsModifier.mouth[props.type].top) * sizeM,
+    )}px;
 `;
 let EarC = styled.View<StyledProps>`
   position: absolute;
-  height: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  width: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  left: ${(props) => PixelRatio.roundToNearestPixel(props.left * sizeM)}px;
-  top: ${(props) => PixelRatio.roundToNearestPixel(props.top * sizeM)}px;
+  height: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.ear[props.type].height) * sizeM,
+    )}px;
+  width: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.ear[props.type].width) * sizeM,
+    )}px;
+  left: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.left + partsModifier.ear[props.type].left) * sizeM,
+    )}px;
+  top: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.top + partsModifier.ear[props.type].top) * sizeM,
+    )}px;
 `;
 let HairC = styled.View<StyledProps>`
   position: absolute;
-  height: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  width: ${(props) => PixelRatio.roundToNearestPixel(props.size * sizeM)}px;
-  left: ${(props) => PixelRatio.roundToNearestPixel(props.left * sizeM)}px;
-  top: ${(props) => PixelRatio.roundToNearestPixel(props.top * sizeM)}px;
+  height: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.hair[props.type].height) * sizeM,
+    )}px;
+  width: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.size + partsModifier.hair[props.type].width) * sizeM,
+    )}px;
+  left: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.left + partsModifier.hair[props.type].left) * sizeM,
+    )}px;
+  top: ${(props) =>
+    PixelRatio.roundToNearestPixel(
+      (props.top + partsModifier.hair[props.type].top) * sizeM,
+    )}px;
 `;
 let AvatarC = styled.View`
   top: ${PixelRatio.roundToNearestPixel(-50 * sizeM)}px;
@@ -75,9 +234,9 @@ let AvatarC = styled.View`
 class Avatar extends Component {
   state = {
     avatar: {
-      hair: { type: 0, size: 85, top: -17, left: 5 },
-      head: { type: 0, size: 100, top: 0, left: 0 },
-      ear: { type: 0, size: 30, top: 35, left: 60 },
+      hair: { color: '#291600', type: 1, size: 85, top: -17, left: 5 },
+      head: { color: '#e6c9a1', type: 0, size: 100, top: 0, left: 0 },
+      ear: { type: 1, size: 30, top: 35, left: 60 },
       eyeL: { type: 0, size: 20, top: 35, left: 15 },
       eyeR: { type: 0, size: 20, top: 35, left: 37 },
       nose: { type: 0, size: 20, top: 50, left: 25 },
@@ -98,27 +257,31 @@ class Avatar extends Component {
             size={avatar.head.size}
             top={avatar.head.top}
             left={avatar.head.left}
+            type={avatar.head.type}
           >
-            <Head />
+            <Head type={avatar.head.type} color={avatar.head.color} />
           </HeadC>
           <HairC
             size={avatar.hair.size}
             top={avatar.hair.top}
             left={avatar.hair.left}
+            type={avatar.hair.type}
           >
-            <Hair />
+            <Hair type={avatar.hair.type} color={avatar.hair.color} />
           </HairC>
           <NoseC
             size={avatar.nose.size}
             top={avatar.nose.top}
             left={avatar.nose.left}
+            type={avatar.nose.type}
           >
-            <Nose />
+            <Nose type={avatar.nose.type} />
           </NoseC>
           <EyeL
             size={avatar.eyeL.size}
             top={avatar.eyeL.top}
             left={avatar.eyeL.left}
+            type={avatar.eyeL.type}
           >
             <Eye type={avatar.eyeL.type} />
           </EyeL>
@@ -126,6 +289,7 @@ class Avatar extends Component {
             size={avatar.eyeR.size}
             top={avatar.eyeR.top}
             left={avatar.eyeR.left}
+            type={avatar.eyeR.type}
           >
             <Eye type={avatar.eyeR.type} />
           </EyeR>
@@ -133,23 +297,19 @@ class Avatar extends Component {
             size={avatar.mouth.size}
             top={avatar.mouth.top}
             left={avatar.mouth.left}
+            type={avatar.mouth.type}
           >
-            <Mouth />
+            <Mouth type={avatar.mouth.type} />
           </MouthC>
           <EarC
             size={avatar.ear.size}
             top={avatar.ear.top}
             left={avatar.ear.left}
+            type={avatar.ear.type}
           >
-            <Ear />
+            <Ear type={avatar.ear.type} />
           </EarC>
         </AvatarC>
-        <Button
-          onPress={this.handleButton}
-          title="Make Eyes Look Like Mouths"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
       </>
     );
   }
