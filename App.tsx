1be8ef51d3318ebc699f91e9d5ce8a8cbce66613
scribhneoir <scribhneoir21@gmail.com>
2021-09-30 09:50:20 -0400

    Revamped avatar moddeling

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import {
 import { Satisfy_400Regular } from '@expo-google-fonts/satisfy';
 import AppLoading from 'expo-app-loading';
 import AvatarCreator from './components/AvatarCreator';
+import NavBar from './components/NavBar';
 import Login from './components/Login';
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -33,6 +34,7 @@ export default function App() {
         top: -17,
         left: 5,
         rot: 0,
+        mode: 0,
       },
       head: {
         color: '#e6c9a1',
@@ -41,41 +43,52 @@ export default function App() {
         top: 0,
         left: 0,
         rot: 0,
+        mode: 0,
       },
       ear: {
+        color: '#e6c9a1',
         type: 2,
         size: 30,
         top: 35,
         left: 60,
         rot: 0,
+        mode: 0,
       },
       eyeL: {
+        color: '#fff',
         type: 0,
         size: 20,
         top: 35,
         left: 15,
         rot: 0,
+        mode: 0,
       },
       eyeR: {
+        color: '#fff',
         type: 0,
         size: 20,
         top: 35,
         left: 37,
         rot: 0,
+        mode: 0,
       },
       nose: {
+        color: '#e6c9a1',
         type: 3,
         size: 20,
         top: 50,
         left: 25,
         rot: 0,
+        mode: 0,
       },
       mouth: {
+        color: '#e6c9a1',
         type: 7,
         size: 20,
         top: 65,
         left: 30,
         rot: 0,
+        mode: 0,
       },
     },
     emailVerification: false,
@@ -85,20 +98,23 @@ export default function App() {
     return <AppLoading />;
   } else {
     return (
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="Login">
-            {(props) => <Login {...props} setUser={setUser} />}
-          </Stack.Screen>
-          <Stack.Screen name="AvatarCreator">
-            {() => <AvatarCreator userID={user._id} avatar={user.avatar} />}
-          </Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="Login">
+              {(props) => <Login {...props} setUser={setUser} />}
+            </Stack.Screen>
+            <Stack.Screen name="AvatarCreator">
+              {() => <AvatarCreator userID={user._id} avatar={user.avatar} />}
+            </Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
+        <NavBar />
+      </>
     );
   }
 }
